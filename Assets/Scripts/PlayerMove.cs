38fca8d243079cc662196f7a88c7ec5b39720b92
Thmaster7 <theorocha72@gmail.com>
2025-03-21 22:01:06 -0300

    Animation

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -7,6 +7,8 @@ public class PlayerMove : MonoBehaviour
     public FixedJoystick joystick;
     public float SpeedMove = 5f;
     private CharacterController controller;
+    public Animator anim;
+    private float x, y;
 
     private void Start()
     {
@@ -16,5 +18,7 @@ public class PlayerMove : MonoBehaviour
     {
         Vector3 Move = transform.right * joystick.Horizontal + transform.forward * joystick.Vertical;
         controller.Move(Move * SpeedMove*Time.deltaTime);
+        anim.SetFloat("VelX", joystick.Horizontal);
+        anim.SetFloat("VelY", joystick.Vertical);
     }
 }

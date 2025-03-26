51064d5d8e72b20d6a833820fafad94c2fe25d0c
Thmaster7 <theorocha72@gmail.com>
2025-03-26 10:42:29 -0300

    MoveChanges

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -6,9 +6,10 @@ public class PlayerMove : MonoBehaviour
 {
     public FixedJoystick joystick;
     public float SpeedMove = 5f;
+    public float rotationSpeed = 1f;
     private CharacterController controller;
     public Animator anim;
-    private float x, y;
+    
 
     private void Start()
     {
@@ -16,9 +17,26 @@ public class PlayerMove : MonoBehaviour
     }
     private void Update()
     {
+        
         Vector3 Move = transform.right * joystick.Horizontal + transform.forward * joystick.Vertical;
         controller.Move(Move * SpeedMove*Time.deltaTime);
         anim.SetFloat("VelX", joystick.Horizontal);
         anim.SetFloat("VelY", joystick.Vertical);
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Moved)
+            {
+                float rotationY = touch.deltaPosition.x * rotationSpeed * Time.deltaTime;
+                transform.Rotate(0, rotationY, 0);
+            }
+        }
+
     }
+    private void HandleRotation()
+    {
+        
+    }
+    
 }

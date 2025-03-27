a072b51f0ac407e47f9fa6e804d74793f9d1363c
Thmaster7 <theorocha72@gmail.com>
2025-03-27 18:10:20 -0300

    Combo Changes

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -12,27 +12,22 @@ public class CameraController : MonoBehaviour
     private bool isOriginalPosition;
     private Vector3 originalOffset;
     private Vector3 reverseOffset = new Vector3(0, 5, -7);
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         originalOffset = offset;
-
     }
-
     // Update is called once per frame
     void Update()
     {
-        if(!player.canAttack)
+        if (player.anim.GetInteger("ComboStep") == 0)
         {
             Vector3 desiredPosition = player.transform.position + player.transform.TransformDirection(offset);
             transform.position = Vector3.Lerp(transform.position, desiredPosition, positionSmoothSpeed * Time.deltaTime);
             Quaternion carRotation = Quaternion.LookRotation(player.transform.position - transform.position);
             transform.rotation = Quaternion.Slerp(transform.rotation, carRotation, rotationSmoothSpeed * Time.deltaTime);
-
             Vector3 currentOffset = Input.GetKey(KeyCode.S) ? reverseOffset : offset;
         }
-
-        
-
     }
 }
